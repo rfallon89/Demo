a8c25591e7e96964ac6666857e5c9b007fccc8b8
rfallon89 <rory_fallon@hotmail.co.uk>
2023-06-06 22:15:11 +0100

    form validation added

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,49 +1,112 @@
-import {useFormik} from 'formik';
 import submit from '../Assets/Icon_Submit.svg'
 import '../Styles/Form.css'
 import { useState } from 'react';
 function Form(){
     const [showAddress,setShowAddress] = useState(false)
     const [addNumber, setAddNumber] = useState(true)
-
-    const data = useFormik({
-        initialValues:{
-            name:'',
-            email:'',
-            phone1:'',
-            phone2:'',
-            message:'',
-            address1:'',
-            address2:'',
-            city:'',
-            region:'',
-            postcode:'',
-            country:'',
+    const [data, setData] = useState({
+        FullName:'',
+        EmailAddress:'',
+        PhoneNumbers:['',''],
+        Message:'',
+        bIncludeAddressDetails:false,
+        AddressLine1:'',
+        AddressLine2:'',
+        CityTown:'',
+        StateCounty:'',
+        Postcode:'',
+        Country:'',
+    })
+    const [validate, setValidate] = useState({
+        name:true,
+        email:true,
+        phone1:true,
+        phone2:true,
+        message:true,
+        AddressLine1:true,
+        AddressLine2:true,
+        CityTown:true,
+        StateCounty:true,
+        Postcode:true,
+        Country:true,
+    })
+    const validateName =()=>{
+        if(!data.FullName.length){
+            setValidate({...validate,name:false})
+        }
+        else{
+            setValidate({...validate,name:true})
         }
     }
-    )
+
+    const validateEmail = () =>{
+        let input = data.EmailAddress 
+        if(input.length || !input.match(/[^\s@]+@[^\s@]+\.[^\s@]+/gi)){
+            setValidate({...validate,email:false})
+        }
+        else{
+            setValidate({...validate,email:true})
+        }
+    }
+
+    const validatePhone = () => {
+        data.PhoneNumbers.forEach((number,index)=>{
+            if(number.length<10 && number.length>20){
+                setValidate({validate,[`phone${index+1}`]:false})
+            }
+        })
+    }
+
+    const validateMessage = () => {
+        if(!data.Message){
+            setValidate({...validate,message:false})
+        }
+    }
+
+    const validateAddress = ({target}) =>{
+        if(!data[`${target.id}`].length){
+            setValidate({...validate,[`${target.id}`]:false})
+        }
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        console.log('here')
+    }
 
     return(
-        <form onSubmit={data.handleSubmit}>
+        <form onSubmit={handleSubmit}>
                 <div className='row'>
                     <div className='column'>
                         <label id="name">Full name</label>
-                        <input className='input-2' id="name" type="text" name="name" onChange={data.handleChange} value={data.values.name}/>
+                        <input className='input-2' id="name" type="text" onChange={({target:{value}})=>setData({...data,FullName:value})} onBlur={validateName} value={data.FullName}/>
+                        <p className={validate.name?'hide':'show'}>*Required</p>
                     </div>
                     <div className='column'>
                         <label id="email">Email address</label>
-                        <input className='input-2' id="email" type="email" name="email" onChange={data.handleChange} value={data.values.email}/>
+                        <input className='input-2' id="email" type="email" onChange={({target:{value}})=>setData({...data,EmailAddress:value})} onBlur={validateEmail} value={data.EmailAddress}/>
+                        <p className={validate.email?'hide':'show'}>*Invalid email address</p>
                     </div>
                 </div>
 
                 <div className='column'>
                 <label id="phone1">Phone number 01 <i>- optional</i></label>
-                <input id="phone1" type="number" name="phone1" onChange={data.handleChange} value={data.values.phone1}/>
+                <input id="phone1" type="tel" onChange={({target:{value}})=>{
+                    const change = {...data}
+                    change.PhoneNumbers[0] = value
+                    setData(change)
+                }} onBlur={validatePhone} value={data.PhoneNumbers[0]}/>
+                <p className={validate.phone1?'hide':'show'}>*Invalid number</p>
                 </div>
 
                 <div className='column'>
                 <label id="phone2" hidden={addNumber} >Phone number 02 <i>- optional</i></label>
-                <input id="phone2" hidden={addNumber} type="number" name="phone2" onChange={data.handleChange} value={data.values.phone2}/>
+                <input id="phone2" hidden={addNumber} type="tel" name="phone2"onChange={({target:{value}})=>{
+                    const change = {...data}
+                    change.PhoneNumbers[1] = value
+                    setData(change)
+                }} onBlur={validatePhone} value={data.PhoneNumbers[1]}/>
+                  <p className={validate.phone2?'hide':'show'}>*Invalid number</p>
                 </div> 
 
                 <button id='phone' disabled={!addNumber} onClick={()=>setAddNumber(false)}>Add new phone number</button>
@@ -53,7 +116,8 @@ function Form(){
                     <label id="message">Message</label>
                     <p id='message-max'>Maximum text length is 500 characters</p>
                     </div>
-                    <textarea id="message" name="message" rows='8' onChange={data.handleChange} value={data.values.message}/>
+                    <textarea id="message" name="message" maxLength='500'rows='8' onChange={({target:{value}})=>setData({...data,Message:value})} onBlur={validateMessage} value={data.Message}/>
+                    <p className={validate.message?'hide':'show'}>*required</p>
                 </div>
 
                     <div>
@@ -66,30 +130,36 @@ function Form(){
                  <div className={!showAddress?'Address-row':'row'}>
                      <div className='column'>
                         <label id="address1">Address line 1</label>
-                        <input className='input-2' id="address1" type="text" name="address1" onChange={data.handleChange} value={data.values.address1}/>
+                        <input className='input-2' id="AddressLine1" type="text" onChange={({target:{value}})=>setData({...data,AddressLine1:value})} onBlur={validateAddress} value={data.AddressLine1}/>
+                        <p className={validate.AddressLine1?'hide':'show'}>*required</p>
                     </div>
                     <div className='column'>
                         <label id="address2">Address line 2</label>
-                        <input className='input-2' id="address2" type="text" name="address2" onChange={data.handleChange} value={data.values.address2}/>
+                        <input className='input-2' id="AddressLine2" type="text" onChange={({target:{value}})=>setData({...data,AddressLine2:value})} onBlur={validateAddress} value={data.AddressLine2}/>
+                        <p className={validate.AddressLine2?'hide':'show'}>*required</p>
                     </div>
                 </div>
 
                 <div className={!showAddress?'Address-row':'row'}>
                     <div className='column'>
                         <label id="city">City</label>
-                        <input className='input-4' id="city" type="text" name="city" onChange={data.handleChange} value={data.values.city}/>
+                        <input className='input-4' id="CityTown" type="text" onChange={({target:{value}})=>setData({...data,CityTown:value})} onBlur={validateAddress} value={data.CityTown}/>
+                        <p className={validate.CityTown?'hide':'show'}>*required</p>
                     </div>
                     <div className='column'>
                         <label id="region">State/County</label>
-                        <input className='input-4' id="region" type="text" name="region" onChange={data.handleChange} value={data.values.region}/>
+                        <input className='input-4' id="StateCounty" type="text" onChange={({target:{value}})=>setData({...data,StateCounty:value})} onBlur={validateAddress} value={data.StateCounty}/>
+                        <p className={validate.StateCounty?'hide':'show'}>*required</p>
                     </div>
                     <div className='column'>
                         <label id="postcode">Postcode</label>
-                        <input className='input-4' id="postcode" type="text" name="postcode" onChange={data.handleChange} value={data.values.postcode}/>
+                        <input className='input-4' id="Postcode" type="text" onChange={({target:{value}})=>setData({...data,Postcode:value})} onBlur={validateAddress} value={data.Postcode}/>
+                        <p className={validate.Postcode?'hide':'show'}>*required</p>
                     </div>
                     <div className='column'>
                         <label id="country">Country</label>
-                        <input className='input-4' id="country" type="text" name="country" onChange={data.handleChange} value={data.values.country}/>
+                        <input className='input-4' id="Country" type="text" name="country" onChange={({target:{value}})=>setData({...data,Country:value})} onBlur={validateAddress} value={data.Country}/>
+                        <p className={validate.Country?'hide':'show'}>*required</p>
                     </div>
                 </div>
 
